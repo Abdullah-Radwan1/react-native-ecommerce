28c6abb2552870ead5516424010e36db5b8fc22c
abdullah <abdallahbeedo855@gmail.com>
2025-05-26 19:34:56 +0300

    Initial commit

---
+++ app/notes/_layout.tsx
@@ -0,0 +1,9 @@
+import { Stack } from "expo-router";
+
+import React from "react";
+
+const NotesLayout = () => {
+  return <Stack screenOptions={{ headerShown: false }}>Notes</Stack>;
+};
+
+export default NotesLayout;
---
+++ app/notes/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from "react-native";
+const NoteScreen = () => {
+  return <View style={styles.container}>NoteScreen</View>;
+};
+
+export default NoteScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
